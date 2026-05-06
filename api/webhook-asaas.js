e67e44b09169ae4d014d21ea8499cf38c0631817
thiagocarvalhod5 <thiagocarvalhod5@gmail.com>
2026-05-06 12:38:09 -0300

    Update webhook-asaas.js

--- api/webhook-asaas.js
+++ api/webhook-asaas.js
@@ -1,7 +1,3 @@
 export default function handler(req, res) {
-  if (req.method === 'POST') {
-    console.log("Recebi algo do Asaas:", req.body);
-    return res.status(200).json({ message: "Recebido com sucesso!" });
-  }
-  return res.status(405).json({ error: "Method Not Allowed" });
+  res.status(200).send("OK");
 }
